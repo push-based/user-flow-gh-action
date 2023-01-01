50c423fb824001aa0ca60bfd079cb1a2fb7d99cb
Michael Hladky <michael@hladky.at>
2023-01-01 01:29:38 +0100

    fix: add compiled JS to repo

---
+++ dist/packages/user-flow-gh-action/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const main_1 = require("./lib/main");
+(0, main_1.run)();
+//# sourceMappingURL=index.js.map
---
+++ dist/packages/user-flow-gh-action/src/lib/user-flow-gh-action.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userFlowGhAction = void 0;
+function userFlowGhAction() {
+    return 'user-flow-gh-action';
+}
+exports.userFlowGhAction = userFlowGhAction;
+//# sourceMappingURL=user-flow-gh-action.js.map
