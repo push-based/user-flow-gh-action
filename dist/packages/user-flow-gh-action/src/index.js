0f0a3c8eb53a32f61c13c365c8462d69b33b8721
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2023-01-03 22:15:02 +0100

    refactor: convert action to Node.js application project

--- dist/packages/user-flow-gh-action/src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const main_1 = require("./lib/main");
-(0, main_1.run)();
-//# sourceMappingURL=index.js.map
--- packages/user-flow-gh-action/src/main.ts
+++ packages/user-flow-gh-action/src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {wait} from './app/wait'
 
 export async function run(): Promise<void> {
   core.debug(`-> wait`)
