fbe4fa3cb6e2952d36027a779591941a087f56a5
Michael Hladky <michael@hladky.at>
2023-01-04 07:10:26 +0100

    test: adopt tests

--- packages/user-flow-gh-action/src/app/executeUFCI.spec.ts
+++ packages/user-flow-gh-action/src/app/executeUFCI.spec.ts
@@ -1,14 +1,14 @@
-import {wait} from './wait'
+import {executeUFCI} from './executeUFCI'
 import {expect, test} from '@jest/globals'
 
 test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+  const input = 42 as unknown as string;
+  await expect(executeUFCI(input)).rejects.toThrow('rcPath is a number')
 })
 
 test('wait 500 ms', async () => {
   const start = new Date()
-  await wait(500)
+  await executeUFCI('user-flowrc.json')
   const end = new Date()
   var delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThan(450)
