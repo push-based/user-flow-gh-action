693a3dc8b0297212b69a39911efe0d3a68cc37f7
Michael Hladky <michael@hladky.at>
2023-01-05 19:08:37 +0100

    refactor: update get inputs

--- packages/user-flow-gh-action/src/app/get-inputs.ts
+++ packages/user-flow-gh-action/src/app/get-inputs.ts
@@ -1,13 +1,24 @@
 // A majority of this code is borrowed from [lhci-gh-action](https://github.com/treosh/lighthouse-ci-action)
 import * as core from '@actions/core';
 import { RcJson, readRcConfig } from '@push-based/user-flow';
-import { get } from 'lodash';
 import { resolve } from 'path';
 
-export function getInput() {
-  // fallback to upload.serverBaseUrl + upload.token for previous API support
-  const serverBaseUrl = core.getInput('serverBaseUrl') || core.getInput('upload.serverBaseUrl')
-  const serverToken = core.getInput('serverToken') || core.getInput('upload.token')
+type GhActionParams = {
+  // global
+  rcPath: string;
+  // collect
+  url: string;
+  // assert
+  // upload
+  serverBaseUrl: string;
+  serverToken: string;
+  basicAuthUsername: string;
+  basicAuthPassword: string;
+}
+
+export function getInput(): GhActionParams {
+  const serverBaseUrl: string = core.getInput('serverBaseUrl');
+  const serverToken: string = core.getInput('serverToken');
 
   // Make sure we don't have UFCI xor API token
   if (!!serverBaseUrl != !!serverToken) {
@@ -16,50 +27,46 @@ export function getInput() {
     process.exit(1)
   }
 
-  const temporaryPublicStorage = core.getInput('temporaryPublicStorage') === 'true' ? true : false
-  if (serverBaseUrl && temporaryPublicStorage) {
-    core.setFailed(`Both UFCI server and Temporary storage are set, choose one upload method.`)
-    process.exit(1)
-  }
-
-  let urls = null
+  let url: string | null = null
 
   // Inspect user-flowrc file for malformations
-  const rcPath = core.getInput('rcPath') ? resolve(core.getInput('rcPath')) : null
+  const rcPath: string | null = core.getInput('rcPath') ? resolve(core.getInput('rcPath')) : null
   if (rcPath) {
-    const rcFileObj: RcJson = readRcConfig(rcPath)
+    const rcFileObj: RcJson = readRcConfig(rcPath);
 
     // Check if we have a static-dist-dir
     if (rcFileObj.collect) {
       if (rcFileObj.collect.url) {
-        urls = rcFileObj.collect.url
+        url = rcFileObj.collect.url
       }
-
     }
+  } else {
+    // Fail and exit
+    core.setFailed(`Need user-flowrc json to run.`)
+    process.exit(1)
   }
 
+
   // Get and interpolate URL's
-  urls = urls || interpolateProcessIntoUrls(getList('urls'))
+  url = interpolateProcessIntoUrls([url])[0];
 
   // Make sure we have either urls or a static-dist-dir
-  if (!urls) {
+  if (!url) {
     // Fail and exit
     core.setFailed(`Need either 'urls' in action parameters or a 'static_dist_dir' in user-flowrc file`)
     process.exit(1)
   }
 
   return {
-    // collect
-    urls,
-    // assert
-    budgetPath: core.getInput('budgetPath') || '',
     rcPath,
+    // collect
+    url,
+    // assert
     // upload
     serverBaseUrl,
     serverToken,
     basicAuthUsername: core.getInput('basicAuthUsername') || 'user-flow',
-    basicAuthPassword: core.getInput('basicAuthPassword'),
-    artifactName: core.getInput('artifactName'),
+    basicAuthPassword: core.getInput('basicAuthPassword')
   }
 }
 
@@ -68,10 +75,10 @@ export function getInput() {
  *
  * @param {string | null} rcPath
  */
-exports.hasAssertConfig = function hasAssertConfig(rcPath) {
+export function hasAssertConfig(rcPath: string): boolean {
   if (!rcPath) return false
   const rcFileObj = readRcConfig(rcPath)
-  return Boolean(get(rcFileObj, 'ci.assert'))
+  return Boolean(rcFileObj.assert)
 }
 
 /**
@@ -91,7 +98,7 @@ function getList(arg, separator = '\n') {
  *
  * @param {string[]} urls
  */
-function interpolateProcessIntoUrls(urls) {
+function interpolateProcessIntoUrls(urls: string[]): string[] {
   return urls.map((url) => {
     if (!url.includes('$')) return url
     Object.keys(process.env).forEach((key) => {
