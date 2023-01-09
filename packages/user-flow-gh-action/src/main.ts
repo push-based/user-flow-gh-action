3a95fc4b1698c3818ee460da8df0b21b81eefb36
Michael Hladky <michael@hladky.at>
2023-01-09 22:22:58 +0100

    refactor: revert main.ts

--- packages/user-flow-gh-action/src/main.ts
+++ packages/user-flow-gh-action/src/main.ts
@@ -1,20 +1,19 @@
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
-import {expect, test} from '@jest/globals'
-import { OUT_PATH } from '../support/constants';
-import * as core from '@actions/core';
+import * as core from '@actions/core'
+import {executeUFCI} from './app/executeUFCI'
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath;
+export async function run(): Promise<void> {
+  core.debug(`-> wait`)
+  try {
+    const rcPath: string = core.getInput('rcPath')
+    core.debug(`rcPath is ${rcPath} ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-  const ip = path.join(process.cwd(), OUT_PATH, 'src', 'index.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
+    core.debug(new Date().toTimeString())
+    await executeUFCI(rcPath)
+    core.debug(new Date().toTimeString())
+
+    core.setOutput('time', new Date().toTimeString())
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
   }
-  const out = cp.execFileSync(np, [ip], options).toString();
-  core.debug('out: '+ out);
-  expect(out.includes('::debug::-> wait')).toBeTruthy();
-})
+}
+
