189737c2d8a4813d09e8cf4134ada7ba26f64b5e
Michael Hladky <michael@hladky.at>
2023-01-09 08:39:07 +0100

    test: adopt main tests

--- packages/user-flow-gh-action/src/main.ts
+++ packages/user-flow-gh-action/src/main.ts
@@ -1,8 +1,16 @@
 import * as core from '@actions/core'
+import { processParamsToParamsArray } from '@push-based/node-cli-testing';
 import {executeUFCI} from './app/executeUFCI'
+import { getInputs } from './app/get-inputs';
+import { runChildCommand } from './app/run-child-command';
+
 
 export async function run(): Promise<void> {
-  core.debug(`-> wait`)
+  core.debug(`getInputs`);
+  const ghActionInputs = getInputs();
+
+ // runChildCommand('collect', processParamsToParamsArray(ghActionInputs));
+
   try {
     const rcPath: string = core.getInput('rcPath')
     core.debug(`rcPath is ${rcPath} ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
