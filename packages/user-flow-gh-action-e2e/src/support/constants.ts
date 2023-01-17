3878891597abf813c8807734004a50ee8bc92cec
Michael Hladky <michael@hladky.at>
2023-01-17 03:01:53 +0100

    docs: add comments

--- packages/user-flow-gh-action-e2e/src/support/constants.ts
+++ packages/user-flow-gh-action-e2e/src/support/constants.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from 'fs';
 import * as path from 'path';
 
+// project.json value
 const { targets } = JSON.parse(readFileSync(path.join(__dirname, '..', '..','..','user-flow-gh-action', 'project.json')).toString());
-
+// path to dist folder
 export const OUT_PATH = targets['build-app'].options.outputPath;
