9a7f65e17543ef6bc4191bf132f620e86b5fe032
Michael Hladky <michael@hladky.at>
2023-01-18 02:07:46 +0100

    refactor: add logs to process results

--- packages/user-flow-gh-action/src/app/process-result.ts
+++ packages/user-flow-gh-action/src/app/process-result.ts
@@ -1,9 +1,15 @@
 import { GhActionInputs } from './types';
 import { RcJson, readRcConfig, createReducedReport } from '@push-based/user-flow';
 import { readdirSync, readFileSync } from 'fs';
+import * as core from '@actions/core';
 
 export function processResult(ghActionInputs: GhActionInputs): string {
+  core.startGroup(`Process result`);
   const rcFileObj: RcJson = readRcConfig(ghActionInputs.rcPath, { fail: true });
-  const result = JSON.parse(readFileSync(readdirSync(rcFileObj.persist.outPath)[0]).toString());
-  return JSON.stringify(createReducedReport(result));
+  const resultStr = readFileSync(readdirSync(rcFileObj.persist.outPath)[0]).toString();
+  core.debug(`Processed results: ${resultStr}`);
+  const reducedResult = JSON.stringify(createReducedReport(JSON.parse(resultStr)));
+  core.debug(`Processed results: ${reducedResult}`);
+  core.endGroup();
+  return reducedResult;
 }
