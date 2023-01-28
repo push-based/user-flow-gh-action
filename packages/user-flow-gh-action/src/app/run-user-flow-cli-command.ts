3aa57f5b49cdad063da361b630fec31d291fcbf3
Michael Hladky <michael@hladky.at>
2023-01-28 17:29:39 +0100

    test: change defaults, ensure CI env var is set

--- packages/user-flow-gh-action/src/app/run-user-flow-cli-command.ts
+++ packages/user-flow-gh-action/src/app/run-user-flow-cli-command.ts
@@ -3,12 +3,18 @@ import * as core from '@actions/core';
 
 export function runUserFlowCliCommand(bin: string, command: 'collect' = 'collect', args: string[] = [], processOptions: { cwd?: string, env?: {} } = {}) {
   const combinedArgs = [bin, command, ...args];
-  const { cwd, env } = processOptions;
+  let { cwd, env } = processOptions;
+  env = env || process.env;
+  // Ensure we run in cliMode "CI"
+  if(env['CI'] === undefined) {
+    env['CI'] = true;
+  }
   const options: ExecSyncOptions = {
     cwd: cwd || process.cwd(),
-    env: env || process.env
+    env
   };
-  core.debug(`CLI process options: ${JSON.stringify(options)}`);
+
+  core.debug(`CLI process options: ${JSON.stringify(options.env.CI)}`);
   // @TODO use childProcess.execSync to get stdout and forward it
   return execSync(combinedArgs.join(' '), options);
 }
