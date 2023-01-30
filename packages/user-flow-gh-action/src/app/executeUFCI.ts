d6fb7bc34df0a3df51b4171d2963ef7ef6a7b22b
Michael Hladky <michael@hladky.at>
2023-01-30 18:31:23 +0100

    refactor: add comments

--- packages/user-flow-gh-action/src/app/executeUFCI.ts
+++ packages/user-flow-gh-action/src/app/executeUFCI.ts
@@ -1,5 +1,6 @@
 import { GhActionInputs } from './types';
 import { runUserFlowCliCommand } from './run-user-flow-cli-command';
+import * as core  from '@actions/core';
 import { processParamsToParamsArray } from './utils';
 
 export async function executeUFCI(
@@ -12,7 +13,12 @@ export async function executeUFCI(
     if (!rcPath) {
       reject('rcPath not given');
     }
-    const res = run('npx @push-based/user-flow', 'collect', processParamsToParamsArray({ rcPath }));
+    const script =  'npx @push-based/user-flow';
+    const command =  'collect';
+    const params =  processParamsToParamsArray({ rcPath });
+    core.debug(`Execute CLI: ${script}${command}${params}`);
+
+    const res = run(script, command, params);
     resolve(res);
   });
 }
