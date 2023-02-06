bccd4b64f203d940f23848aa0057388833c94afd
Michael Hladky <michael@hladky.at>
2023-02-06 16:00:43 +0100

    test: ajdust main.ts 3

--- packages/user-flow-gh-action/src/main.ts
+++ packages/user-flow-gh-action/src/main.ts
@@ -9,31 +9,35 @@ import { GhActionInputs } from './app/types';
 
 export async function run(): Promise<void> {
   core.debug(`Run main`);
+  try {
+    core.startGroup(`Get inputs form action.yml`);
+    let ghActionInputs: GhActionInputs = getInputs();
+    core.endGroup();
 
-  core.startGroup(`Get inputs form action.yml`);
-  let ghActionInputs: GhActionInputs = getInputs();
-  core.endGroup();
+    core.startGroup(`Execute user-flow`);
+    // @TODO retrieve result
+    await executeUFCI(ghActionInputs);
+    core.endGroup();
 
-  core.startGroup(`Execute user-flow`);
-  // @TODO retrieve result
-  await executeUFCI(ghActionInputs);
-  core.endGroup();
+    core.startGroup(`Validate results`);
+    const rcFileObj = readJsonFileSync(ghActionInputs.rcPath);
+    const allResults = readdirSync(rcFileObj.persist.outPath);
+    if (!allResults.length) {
+      throw new Error(`No results present in folder ${rcFileObj.persist.outPath}`);
+    }
+    const resPath = join(rcFileObj.persist.outPath, allResults[0]);
+    core.endGroup();
 
-  core.startGroup(`Validate results`);
-  const rcFileObj = readJsonFileSync(ghActionInputs.rcPath);
-  const allResults = readdirSync(rcFileObj.persist.outPath);
-  if (!allResults.length) {
-    throw new Error(`No results present in folder ${rcFileObj.persist.outPath}`);
+    core.startGroup(`Process results`);
+    const { resultSummary } = processResult(ghActionInputs);
+    core.setOutput('resultPath', resPath);
+    core.setOutput('resultSummary', resultSummary);
+    core.endGroup();
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message);
+    process.exitCode = 1;
+    process.exit(1);
   }
-  const resPath = join(rcFileObj.persist.outPath, allResults[0]);
-  core.endGroup();
-
-  core.startGroup(`Process results`);
-  const { resultSummary } = processResult(ghActionInputs);
-  core.setOutput('resultPath', resPath);
-  core.setOutput('resultSummary', resultSummary);
-  core.endGroup();
-
 }
 
 function exit(error: any) {
