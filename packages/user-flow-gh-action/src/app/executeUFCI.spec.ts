32a26b6867bcf723399caaf1443555531d5e7afb
Michael Hladky <michael@hladky.at>
2023-02-13 12:40:02 +0100

    test: adopt tests 2

--- packages/user-flow-gh-action/src/app/executeUFCI.spec.ts
+++ packages/user-flow-gh-action/src/app/executeUFCI.spec.ts
@@ -1,7 +1,24 @@
 import { executeUFCI } from './executeUFCI';
 import { expect, test } from '@jest/globals';
 import { processParamsToParamsArray } from './utils';
+import { withProject } from '@push-based/node-cli-testing';
+import { DEFAULT_RC_NAME } from '@push-based/user-flow';
+import { join } from "path";
 
+const rootFolder = 'user-flow-gh-action-e2e';
+const rootPath = join(__dirname,'..','..','..',rootFolder);
+const RcJSON = {
+  collect: {
+    url: 'test'
+  }
+};
+let prjCfg = {
+  root: rootPath,
+  bin: '',
+  rcFile: {
+    [DEFAULT_RC_NAME]: RcJSON
+  }
+};
 
 describe('executeUFCI mock', () => {
 
@@ -10,8 +27,8 @@ describe('executeUFCI mock', () => {
     expect(executeUFCI(p)).rejects.toEqual('rcPath not given');
   });
 
-  test('is call with run inside', async () => {
-    const rcPath = 'user-flowrc.json';
+  test('is call with run inside', withProject(prjCfg ,async () => {
+    const rcPath = '.user-flowrc.json';
     const run = (bin: string, command: 'init' | 'collect', args: string[]) => {
       return `Execute CLI: npx @push-based/user-flow collect ${args.join(', ')}` as any;
     };
@@ -20,6 +37,6 @@ describe('executeUFCI mock', () => {
     const res = await executeUFCI((params), run);
     const paramsFormatted =  processParamsToParamsArray({ rcPath, verbose: true, dryRun: false, format: ['md'] });
     expect(res).toBe(`Execute CLI: npx @push-based/user-flow collect ${paramsFormatted.join(', ')}`);
-  });
+  }));
 
 });
--- packages/user-flow-gh-action/src/app/executeUFCI.ts
+++ packages/user-flow-gh-action/src/app/executeUFCI.ts
@@ -1,7 +1,7 @@
 import { GhActionInputs } from './types';
 import { runUserFlowCliCommand } from './run-user-flow-cli-command';
 import * as core  from '@actions/core';
-import { processParamsToParamsArray } from './utils';
+import { processParamsToParamsArray, readJsonFileSync } from './utils';
 
 export async function executeUFCI(
   ghActionInputs: GhActionInputs,
@@ -10,14 +10,19 @@ export async function executeUFCI(
 ): Promise<string> {
   const { rcPath, verbose, dryRun, ...unusedInputs } = ghActionInputs;
   return new Promise((resolve, reject) => {
+
+    // as we need md format for the comment we have to ensure is is included
     if (!rcPath) {
-      reject('rcPath not given');
+      reject(`rcPath ${rcPath} not given`);
     }
-    const params =  { rcPath, verbose, dryRun };
+    const rcFileObj: any = readJsonFileSync(rcPath);
+    const { persist } = rcFileObj;
+    ghActionInputs.format =  Array.from(new Set(['md'].concat(persist?.format || []).concat(ghActionInputs?.format || [])));
+
 
     const script =  'npx @push-based/user-flow';
     const command =  'collect';
-    const processedParams =  processParamsToParamsArray({ rcPath, verbose, dryRun, format: ['md'] });
+    const processedParams =  processParamsToParamsArray(ghActionInputs);
     core.debug(`Execute CLI: ${script} ${command} ${processedParams.join(', ')}`);
 
     const res = run(script, command, processedParams);
