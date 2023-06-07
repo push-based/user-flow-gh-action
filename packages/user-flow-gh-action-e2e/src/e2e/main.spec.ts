3dc6457a24236f0dd96156678c763c89c33a6b8b
Michael <michael.hladky@push-based.io>
2023-06-07 00:54:50 -0400

    fix: custom script test main

--- packages/user-flow-gh-action-e2e/src/e2e/main.spec.ts
+++ packages/user-flow-gh-action-e2e/src/e2e/main.spec.ts
@@ -6,16 +6,15 @@ describe('main.js', () => {
 
   test('runs', withProject<any>({
     ...REMOTE_PRJ_CFG, env: {
-      INPUT_CUSTOMSCRIPT: "xxxxx @push-based/user-flow",
+      INPUT_CUSTOMSCRIPT: "npx @push-based/user-flow",
       INPUT_RCPATH: REMOTE_RC_NAME,
       INPUT_FORMAT: 'html,json'
     }
   }, async (prj :any) => {
     const { stdout, stderr, exitCode } = await prj.exec();
 
-
-    expect(stdout).toContain('!!!!!!!!');
     expect(stdout).toContain('Run main');
+    expect(stdout).toContain('CLI over custom script:');
     expect(stdout).toContain('Get inputs form action.yml');
     expect(stdout).toContain('Execute user-flow');
     expect(stdout).toContain('--format=md');
