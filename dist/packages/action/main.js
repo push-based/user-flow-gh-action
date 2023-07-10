2641bd82c370e0a281c31d623b0a6807ecc48881
push-based.io <opensource@push-based.io>
2023-07-10 21:19:50 +0000

    Persist action build

--- dist/packages/action/main.js
+++ dist/packages/action/main.js
@@ -4,6 +4,7 @@ var __webpack_exports__ = {};
  * This package is just a wrapper action for:
  * - user-flow action located in this repository under `packages/user-flow-gh-action`
  * - sticky-pull-request-comment in GitHub under `marocchino/sticky-pull-request-comment@v2`
+ *
  */
 
 var __webpack_export_target__ = exports;
