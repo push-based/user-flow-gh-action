4513bd3b0009593d1d47d4982c5f2fc4d700b163
Michael <michael.hladky@push-based.io>
2023-07-10 23:22:29 +0200

    fix: release

--- packages/action/src/main.ts
+++ packages/action/src/main.ts
@@ -1,6 +1,7 @@
 /**
  * This package is just a wrapper action for:
- * - user-flow action located in this repository under `packages/user-flow-gh-action`
- * - sticky-pull-request-comment in GitHub under `marocchino/sticky-pull-request-comment@v2`
+ * - user-flow action located in this repository under `packages/user-flow-gh-action`.
+ * - sticky-pull-request-comment in GitHub under `marocchino/sticky-pull-request-comment@v2`.
  *
  */
+const EMPTY_VAR = null;
